248f2c42195a40e1002d9f14f6939aed2f7ea2a7
XRLAB1 <>
2022-11-08 10:00:58 +0100

    working gyro

---
+++ Assets/GyroController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logger : MonoBehaviour
+{
+    private GameObject board;
+    private float initial = 0f;
+    private bool _isDefaultSet = false;
+    void Start()
+    {
+        board = GameObject.FindWithTag("Board");
+    }
+    void Update()
+    {
+
+        Vector3 controllerValues = gameObject.transform.eulerAngles; 
+        
+        if (controllerValues.z != 0f && !_isDefaultSet)
+        {
+            initial = controllerValues.z;
+            Debug.Log(initial + " -------------INITIAL");
+            _isDefaultSet = true;
+        }
+
+        var controllerZ = controllerValues.z;
+        Debug.Log("z: " + gameObject.transform.eulerAngles);
+        if (!_isDefaultSet || controllerZ == 0)
+        {
+            return;
+        }
+        
+        float toRotateBy = initial - controllerZ;
+        // Debug.Log("rotateBy: " + toRotateBy);
+        board.transform.rotation = Quaternion.Euler(0, 0,toRotateBy);
+ }
+}
